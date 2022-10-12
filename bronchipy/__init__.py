0b46d43dd3a6ba667d588c0d84ab38c990c0706f
id-b3 <58172188+id-b3@users.noreply.github.com>
2022-10-12 16:42:03 +0200

    Corrected module init files.

--- bronchipy/__init__.py
+++
@@ -1,1 +0,0 @@
-from airwaytree import AirwayTree
--- bronchipy/calc/__init__.py
+++ bronchipy/calc/__init__.py
@@ -1,3 +1,3 @@
-from measure_airways import calc_branch_length, calc_local_orientations, calc_smoothing, calc_tapering
-from summary_stats import fractal_dimension, agg_param, calc_pi10
-from visualise import save_pi10_figure
+from .measure_airways import calc_branch_length, calc_local_orientations, calc_smoothing, calc_tapering
+from .summary_stats import fractal_dimension, agg_param, calc_pi10
+from .visualise import save_pi10_figure
