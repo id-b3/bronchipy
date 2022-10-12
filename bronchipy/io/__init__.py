0d30a31b5a16a3bd777e333608abe23e0cf90dc1
id-b3 <58172188+id-b3@users.noreply.github.com>
2022-10-12 14:50:34 +0200

    Initial Commit.

---
+++ bronchipy/io/__init__.py
@@ -0,0 +1,1 @@
+from branchio import load_tree_csv, load_pickle_tree, save_as_csv, save_pickle_tree, save_summary_csv
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Supporting tools for processing the output of the AirFlow'\
+    ' pipeline.'
+LONG_DESCRIPTION = 'Scripts and functions to process the output of AirFlow'\
+    ' pipeline, for use with the output files. Allows for post-processing'\
+    ' of the data tables.'
+
+# Setting up
+setup(name="bronchipy",
+      version=VERSION,
+      author="ImaLife",
+      author_email="<i.dudurych@umcg.nl>",
+      description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      long_description=LONG_DESCRIPTION,
+      packages=find_packages(),
+      install_requires=['nibabel==3.1.0', 'pydicom==1.4.2', 'numpy==1.19.2',
+                        'pandas==1.0.3', 'scipy==1.5.3',
+                        'scikit-learn==0.23.1', 'matplotlib==3.3.4',
+                        'SimpleITK==1.2.4', 'scikit-image==0.17.2'],
+      keywords=[
+          'python', 'analysis', 'medical', 'imaging', 'research'
+      ],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Science/Research",
+          "Programming Language :: Python :: 3",
+          "Operating System :: Unix",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+      ])
